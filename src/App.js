0e88fb0e8e8219307533b9135dbcfa02e732a40c
rii92 <riofebri16@gmail.com>
2023-08-11 11:29:57 +0700

    config firebase deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Start Project Story Telling With Data.
         </p>
         <a
           className="App-link"
