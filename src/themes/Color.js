dfb55d76366cf91a5f2de861fc56b45cf2830bff
Muhammad  Abdurrofi <muhrofi01@gmail.com>
2023-08-29 15:04:41 +0700

    ganti color pallete

--- src/themes/Color.js
+++ src/themes/Color.js
@@ -2,11 +2,11 @@ const color = {
   white: "#F6F4EB",
   black: "#181823",
   grey: "#D8D9DA",
-  blue: "#6AA5D9",
-  lightBlue: "#A9C6D9",
-  lightGreen: "#B0D973",
-  semiDarkYellow: "#BABF24",
-  darkYellow: "#A68B03",
+  blue: "#219EBC",
+  lightBlue: "#8ECAE6",
+  darkBlue: "#023047",
+  yellow: "#FFB703",
+  orange: "#FB8500",
 };
 
 export default color;
