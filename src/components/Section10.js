8b8b64d724b774ce45150ba3dd73f30f805af325
Muhammad  Abdurrofi <muhrofi01@gmail.com>
2023-09-13 19:58:51 +0700

    perbaikan pada tampilan section bandung

--- src/components/Section10.js
+++ src/components/Section10.js
@@ -1,46 +1,86 @@
 import { Box, Typography } from "@mui/material";
-import { Parallax } from "react-parallax";
 import color from "../themes/Color";
 
 const Section10 = () => {
   return (
-    <Parallax
-      blur={{ min: -10, max: 10 }}
-      bgImage={"assets/image/bandung.jpg"}
-      bgImageAlt="the dog"
-      strength={400}
+    <Box
+      sx={{
+        position: { xs: "relative" },
+        zIndex: { xs: 15 },
+        backgroundColor: { xs: color.white },
+      }}
     >
       <Box
         sx={{
           width: { xs: "100%" },
           height: { xs: "100vh" },
-          position: { xs: "relative" },
-          zIndex: { xs: 15 },
-          display: { xs: "grid" },
-          gridTemplateColumns: { xs: "1fr", sm: "1fr 1fr" },
-          placeItems: { xs: "center" },
+          paddingX: { xs: "16px" },
+          display: { xs: "flex" },
+          flexDirection: { xs: "column-reverse" },
+          gap: { xs: "24px" },
+          justifyContent: { xs: "center" },
+          alignItems: { xs: "center" },
         }}
       >
         <Typography
           sx={{
-            width: { xs: "fit-content" },
-            padding: { xs: "8px 16px" },
+            width: { xs: "100%", sm: "400px", md: "500px" },
             color: { xs: color.black },
-            backgroundColor: { xs: color.white },
-            borderRadius: { xs: "8px" },
           }}
         >
-          Di Bandung
+          Mendengar kabar terkait asap dari TPA Sarimukti yang terbakar, satria
+          khawatir karena asap tersebut dapat menutupi pandangan pengendara.
+          Namun, Asep meyakinkannya untuk tetap melanjutkan perjalanan.
         </Typography>
-        <Box sx={{ width: { xs: "300px", sm: "350px", md: "400px" } }}>
+        <Box
+          sx={{
+            height: { xs: "300px", sm: "350px", md: "400px" },
+            position: { xs: "relative" },
+          }}
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          <Box
+            sx={{
+              width: { xs: "80px" },
+              position: { xs: "absolute" },
+              left: { xs: "-40px" },
+            }}
+            data-aos="fade-right"
+            data-aos-duration="1000"
+            data-aos-delay="1000"
+          >
+            <img
+              src="assets/image/bubble-question-mark-left.png"
+              alt="Didin"
+              style={{ width: "100%" }}
+            />
+          </Box>
+          <Box
+            sx={{
+              width: { xs: "100px" },
+              position: { xs: "absolute" },
+              top: { xs: "20px" },
+              right: { xs: "-50px" },
+            }}
+            data-aos="fade-left"
+            data-aos-duration="1000"
+            data-aos-delay="1000"
+          >
+            <img
+              src="assets/image/bubble-question-mark-right.png"
+              alt="Didin"
+              style={{ width: "100%" }}
+            />
+          </Box>
           <img
-            src="assets/image/didin.png"
+            src="assets/image/satria/satria-is-thinking.png"
             alt="Didin"
-            style={{ width: "100%" }}
+            style={{ height: "100%" }}
           />
         </Box>
       </Box>
-    </Parallax>
+    </Box>
   );
 };
 
